5558b98b44ab91a312a701b6a7eb7583f0ef3eae
vinayakabhin <agvinayak60@gmail.com>
2026-03-07 00:03:03 +0530

    fixed render deployment

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const cors = require('cors');
 const path = require('path');
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors());
@@ -13,7 +13,8 @@ app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Initialize SQLite Database
-const db = new sqlite3.Database('./foodigo.db', (err) => {
+const dbPath = path.join(__dirname, 'foodigo.db');
+const db = new sqlite3.Database(dbPath, (err) => {
     if (err) {
         console.error('Error opening database:', err.message);
     } else {
